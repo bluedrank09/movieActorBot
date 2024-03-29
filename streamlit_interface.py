f9463eab0690f072723f47657911cbce14d5ddcf
mayababu <mayababu73@gmail.com>
2024-03-29 16:07:10 +1300

    made api and id os.envioron based, added comments

---
+++ streamlit_interface.py
@@ -0,0 +1,39 @@
+# libraries
+import streamlit as st
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+st.title('lol')
+name = st.text_input('Name of actor or movie :')
+st.checkbox('Cast List')
+st.checkbox('Box Office')
+st.checkbox('Fun Trivia')
+st.checkbox('List of movies')
+st.checkbox('Statistics')
+st.checkbox('Fun Facts')
+st.button('Submit')
+
+def main():
+    try:
+        # making the varibales in .env os variables
+        load_dotenv()
+        # making then program variables
+        GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+        GOOGLE_SEARCH_ENGINE_ID = os.environ.get('GOOGLE_SEARCH_ENGINE_ID')
+        # printing
+        print(f"{GOOGLE_API_KEY=}")
+        print(f"{GOOGLE_SEARCH_ENGINE_ID=}")
+    except Exception as error:
+        raise error
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as error:
+        print(error)    
+        raise error
+    finally:
+        print(f":D")
+    
+        
